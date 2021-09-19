872398e3b0157c1064ef776b542f52821c4c60a5
user <git49359@gmail.com>
2021-09-19 20:18:50 +0900

    second commit

---
+++ app/src/main/java/com/ahpuh79815/kotluck/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ahpuh79815.kotluck
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
